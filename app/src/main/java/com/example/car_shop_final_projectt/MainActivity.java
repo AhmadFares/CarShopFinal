59cbd31cea6c613acb9480bf3778c30e2d1fbc9b
Ahmad Fares <98591677+AhmadFares@users.noreply.github.com>
2022-04-15 23:27:48 +0300

    Sign in page design and intents between both activities

--- app/src/main/java/com/example/car_shop_final_projectt/MainActivity.java
+++ app/src/main/java/com/example/car_shop_final_projectt/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.car_shop_final_projectt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button SignInPage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SignInPage= (Button)findViewById(R.id.tosigninpage);
+
+        SignInPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(MainActivity.this,SignInActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/car_shop_final_projectt/SignInActivity.java
+++ app/src/main/java/com/example/car_shop_final_projectt/SignInActivity.java
@@ -2,13 +2,25 @@ package com.example.car_shop_final_projectt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignInActivity extends AppCompatActivity {
 
+    Button SignUpPage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        SignUpPage=(Button) findViewById(R.id.tosigninpage2);
+        SignUpPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(SignInActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
